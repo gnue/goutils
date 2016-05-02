91b283683556ede32323df41327bfe6f4e1dd729
gnue <gnue@so-kukan.com>
2016-05-02 17:11:36 +0900

    parser

---
+++ parser/parser.go
@@ -0,0 +1,114 @@
+package parser
+
+import ()
+
+type callbackFn func(key, val string)
+
+type parser struct {
+	lexer    *lexer
+	item     item
+	err      error
+	callback callbackFn
+}
+
+func NewParser(input string) *parser {
+	return &parser{lexer: lex(input)}
+}
+
+func (p *parser) next() {
+	if p.item.token == itemEOF {
+		return
+	}
+
+	for {
+		p.item = p.lexer.nextItem()
+		if p.item.token != itemComment {
+			break
+		}
+	}
+}
+
+func (p *parser) accept(token rune) bool {
+	if p.item.token == token {
+		p.next()
+		return true
+	}
+
+	return false
+}
+
+func (p *parser) expect(token rune) bool {
+	if p.accept(token) {
+		return true
+	}
+
+	p.err = &expectError{token}
+	return false
+}
+
+func (p *parser) eof() bool {
+	if p.item.token == itemEOF {
+		return true
+	}
+
+	return false
+}
+
+func (p *parser) Parse(callback callbackFn) error {
+	p.callback = callback
+	p.next()
+
+	for p.statement() || p.ignore() {
+	}
+
+	err := p.err
+	if err == nil && !p.eof() {
+		err = &parseError{p.item.val}
+	}
+
+	return err
+}
+
+func (p *parser) ignore() bool {
+	ok := false
+
+	for p.accept('\n') {
+		ok = true
+	}
+
+	return ok
+}
+
+func (p *parser) statement() bool {
+	p.accept(itemExport)
+
+	k := p.item
+	if p.key() {
+		if p.accept('=') || p.accept(':') {
+			s := ""
+
+			v := p.item
+			if p.value() {
+				s = v.value()
+			}
+
+			if p.callback != nil {
+				p.callback(k.value(), s)
+			}
+		}
+
+		if p.accept('\n') || p.accept(itemEOF) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (p *parser) key() bool {
+	return p.accept(itemIdentifier) || p.accept(itemString)
+}
+
+func (p *parser) value() bool {
+	return p.accept(itemText) || p.accept(itemString)
+}
